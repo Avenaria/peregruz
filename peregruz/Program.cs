51fb9f6005d639312a06865d1a669c529e7ab6e0
1 <1@LAPTOP-8U2CR0NO>
2023-07-04 19:16:58 +0300

    Добавьте файлы проекта.

---
+++ peregruz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peregruz
+{
+    class Peregruz
+    {
+        public int x, y;
+        public Peregruz (int x, int y)
+        {
+            this.x = x; 
+            this.y = y;
+        }
+        public static Peregruz operator +(Peregruz a, Peregruz b)=>// перегрузка +
+            new Peregruz(a.x + b.y, a.y + b.x);
+
+        public static Peregruz operator -(Peregruz a, Peregruz b) =>// перегрузка -
+            new Peregruz(a.x - b.y, a.y - b.x);
+        public static bool operator ==(Peregruz a, Peregruz b)
+        {
+            if (a == null || b == null) return false;
+
+            return true;
+            
+        }
+        public static bool operator !=(Peregruz a, Peregruz b)
+        {
+            if (a == null || b == null) return true;
+
+            return false;
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
